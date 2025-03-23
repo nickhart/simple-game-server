0ea5d2ee7a18253d4b84ef7da5df497642479afd
Nicholas Hart <nickhart@gmail.com>
2025-03-23 15:20:57 -0700

    attempt to ensure we skip DB tests in GitHub runner

--- test/unit_helper.rb
+++ test/unit_helper.rb
@@ -1,6 +1,19 @@
 ENV["RAILS_ENV"] = "test_unit"
+ENV["DISABLE_DATABASE"] = "true"
+
 require "minitest/autorun"
+require "active_support"
+require "active_support/test_case"
 require "active_support/testing/autorun"
+
+# Prevent database connection attempts
+module ActiveRecord
+  class Base
+    def self.establish_connection(*args)
+      # No-op to prevent database connections
+    end
+  end
+end
 
 class UnitTest < Minitest::Test
   # Add any shared test helper methods here
