4cb6033827dd0c58ba703bd2e37f2e0d45da4170
Nick Hart <nickhart@gmail.com>
2025-03-23 18:09:55 -0700

    added code coverage, SECURITY.md, and some additional safety/protection
    * added SECURITY.md and some additional safety to the repo
    
    * added my email to the SECURITY doc
    
    * fixed missing simplecov dependency and lint issues
    
    * fixed test coverage issues
    
    * fixed lint errors

--- test/models/game_session_state_test.rb
+++ test/models/game_session_state_test.rb
@@ -20,6 +20,15 @@ class GameSessionStateTest < ActiveSupport::TestCase
 
     @game_session.status = :finished
     assert_not @game_session.save, "Should not allow transition from waiting to finished"
+
+    # Test transition from active to waiting
+    @game_session.game_players.create!(player: @player1)
+    @game_session.game_players.create!(player: @player2)
+    @game_session.status = :active
+    assert @game_session.save, "Should transition to active"
+
+    @game_session.status = :waiting
+    assert_not @game_session.save, "Should not allow transition from active back to waiting"
   end
 
   test "should handle state transitions correctly" do
@@ -59,5 +68,33 @@ class GameSessionStateTest < ActiveSupport::TestCase
 
     assert @game_session.finish_game, "Should finish game successfully"
     assert @game_session.finished?, "Game should be finished after calling finish_game"
+  end
+
+  test "should handle edge cases in state transitions" do
+    # Test nil status_was case
+    new_game = GameSession.new(min_players: 2, max_players: 4)
+    new_game.status = :waiting
+    assert new_game.valid?, "Should allow setting initial status"
+
+    # Test setting status to nil
+    @game_session.write_attribute(:status, nil)
+    assert_not @game_session.valid?, "Should not allow nil status"
+    assert_includes @game_session.errors[:status], "can't be blank"
+
+    # Test setting status to invalid value
+    assert_raises(ArgumentError) do
+      @game_session.status = :invalid_status
+    end
+  end
+
+  test "should handle current_player_index initialization" do
+    assert @game_session.save
+    @game_session.game_players.create!(player: @player1)
+    @game_session.game_players.create!(player: @player2)
+
+    assert_nil @game_session.current_player_index, "Should start with nil current_player_index"
+    @game_session.status = :active
+    assert @game_session.save, "Should transition to active"
+    assert_equal 0, @game_session.current_player_index, "Should initialize current_player_index to 0"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,27 @@
+require "simplecov"
+require "simplecov-cobertura"
+
+SimpleCov.start "rails" do
+  enable_coverage :branch
+
+  add_filter "/bin/"
+  add_filter "/db/"
+  add_filter "/test/"
+
+  add_group "Controllers", "app/controllers"
+  add_group "Models", "app/models"
+  add_group "Services", "app/services"
+
+  SimpleCov.minimum_coverage 90
+  SimpleCov.minimum_coverage_by_file 80
+
+  if ENV["CI"]
+    formatter SimpleCov::Formatter::CoberturaFormatter
+  else
+    formatter SimpleCov::Formatter::HTMLFormatter
+  end
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
